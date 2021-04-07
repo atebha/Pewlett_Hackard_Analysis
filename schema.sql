6b1df1e656a2e58d2c110774c0850b76461aaa26
Ahsan <50600624+atebha@users.noreply.github.com>
2021-04-06 23:37:18 -0700

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -37,3 +37,24 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
+
+-- Departments titles
+CREATE TABLE titles(
+    emp_no INT NOT NULL,
+    title varchar(40) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date NOT NULL,
+    FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY(emp_no, title, from_date, to_date)
+);
+
+-- dept for emp
+CREATE TABLE dept_emp(
+    emp_no INT NOT NULL,
+    dept_no varchar(4) NOT NULL,
+    from_date NOT NULL,
+    to_date NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departemnts (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
